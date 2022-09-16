782882bb84b59c060d419a292559c3f1f7bb8ace
benjym <benjy.marks@sydney.edu.au>
2022-09-16 00:52:30 +0000

    Deploying to gh-pages from @ benjym/granular-vr@52d772bbef855c6b4d37c9f6764b91c1c856f9b6 🚀

---
+++ js/triaxial.js
@@ -0,0 +1,15 @@
+import css from "../css/main.css";
+
+import * as THREE from 'three';
+
+console.debug('Using threejs version ' + THREE.REVISION)
+
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import ImmersiveControls from '@depasquale/three-immersive-controls';
+
+import * as SPHERES from "./SphereHandler.js"
+import * as WALLS from "./WallHandler.js"
+
+export let params = {
+    
+}
