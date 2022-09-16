52d772bbef855c6b4d37c9f6764b91c1c856f9b6
Benjamin Marks <benjy.marks@sydney.edu.au>
2022-09-16 10:51:52 +1000

    added missing files

---
+++ js/buttons.js
@@ -0,0 +1,95 @@
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
+import * as CONTROLLERS from "./controllers";
+
+let fontsize = 0.25;
+let loader = new FontLoader();
+let font;
+loader.load( '../resources/helvetiker_bold.typeface.json', function ( f ) {
+    // console.log('FONT DEFINED!!')
+    font = f;
+});
+
+export function add_url_button(url, name, location, scale, controls, scene) {
+    if ( font !== undefined ) {
+
+        let background;
+
+        var mat = new THREE.MeshStandardMaterial( { color: 0xe72564 } );
+        var geom = new TextGeometry( name, { font: font, size: fontsize, height: fontsize/5., } );
+        var text = new THREE.Mesh( geom, mat );
+        text.geometry.computeBoundingBox();
+
+        text.position.y = -text.geometry.boundingBox.max.y/2.;
+        text.position.x = -text.geometry.boundingBox.max.x/2.;
+        text.position.z = 0.01;
+        text.userData.url = url;
+    
+        var mat = new THREE.MeshStandardMaterial( { color: 0x333333 } );
+        var geom = new THREE.BoxGeometry( fontsize+text.geometry.boundingBox.max.x,fontsize+text.geometry.boundingBox.max.y, 0.1 );
+        background = new THREE.Mesh( geom, mat );
+        background.userData.url = url;
+    
+        background.position.set(...location);
+        background.add(text);
+    
+        const type = 'button';
+        background.userData.type = type; // this sets up interaction group for controllers
+        text.userData.type = type; // this sets up interaction group for controllers
+    
+        controls.interaction.selectStartHandlers[type] = CONTROLLERS.onRedirectButtonSelectStart;
+        controls.interaction.selectEndHandlers[type] = CONTROLLERS.onRedirectButtonSelectEnd;
+        // controls.interaction.selectableObjects.push(button);
+        controls.interaction.selectableObjects.push(background);
+        
+        background.scale.set(scale,scale,scale);
+    
+        scene.add( background );
+
+        return background
+    }
+    else{
+        // console.log('font not loaded, waiting...')
+        setTimeout(add_url_button.bind(null, url, name, location, scale, controls, scene), 200);
+    }
+}
+
+export function add_action_button(type, name, selectStartFunction, selectEndFunction, location, scale, controls, scene) {
+    if ( font !== undefined ) {
+        let background;
+
+        var mat = new THREE.MeshStandardMaterial( { color: 0xe72564 } );
+        var geom = new TextGeometry( name, { font: font, size: fontsize, height: fontsize/5., } );
+        var text = new THREE.Mesh( geom, mat );
+        text.geometry.computeBoundingBox();
+
+        text.position.y = -text.geometry.boundingBox.max.y/2.;
+        text.position.x = -text.geometry.boundingBox.max.x/2.;
+        text.position.z = 0.01;
+
+        var mat = new THREE.MeshStandardMaterial( { color: 0x333333 } );
+        var geom = new THREE.BoxGeometry( fontsize+text.geometry.boundingBox.max.x,fontsize+text.geometry.boundingBox.max.y, 0.1 );
+        background = new THREE.Mesh( geom, mat );
+
+        background.position.set(...location);
+        background.add(text);
+
+        background.userData.type = type; // this sets up interaction group for controllers
+        text.userData.type = type; // this sets up interaction group for controllers
+
+        controls.interaction.selectStartHandlers[type] = selectStartFunction;
+        controls.interaction.selectEndHandlers[type] = selectEndFunction;
+        // controls.interaction.selectableObjects.push(button);
+        controls.interaction.selectableObjects.push(background);
+        
+        background.scale.set(scale,scale,scale);
+
+        scene.add( background );
+
+        return background;
+    }
+    else{
+        // console.log('font not loaded, waiting...')
+        setTimeout(add_url_button.bind(null, url, name, location, scale, controls, scene), 200);
+    }
+}
---
+++ todo.md
@@ -0,0 +1,6 @@
+# To do
+[ ] Get boundaries working for isotropic case
+[ ] What to do about graphing data?
+[X] Add "Back to main menu" buttons to isotropic and triaxial
+[ ] Add acoustic emissions
+[ ] Add controls!
