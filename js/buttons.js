861933a723994523b07d3c13b51b19366e45d91e
Benjy Marks <benjy.marks@sydney.edu.au>
2022-09-20 19:32:17 +1000

    hopefully added font to dist

--- js/buttons.js
+++ js/buttons.js
@@ -5,71 +5,72 @@ import * as CONTROLLERS from "./controllers";
 let fontsize = 0.25;
 let loader = new FontLoader();
 export let font;
-loader.load( '../../resources/helvetiker_bold.typeface.json', function ( f ) {
+loader.load('../resources/helvetiker_bold.typeface.json', function (f) {
+    // loader.load('resources/helvetiker_bold.typeface.json', function (f) {
     // console.log('FONT DEFINED!!')
     font = f;
 });
 
 export function add_url_button(url, name, location, scale, controls, scene) {
-    if ( font !== undefined ) {
+    if (font !== undefined) {
 
         let background;
 
-        var mat = new THREE.MeshStandardMaterial( { color: 0xe72564 } );
-        var geom = new TextGeometry( name, { font: font, size: fontsize, height: fontsize/5., } );
-        var text = new THREE.Mesh( geom, mat );
+        var mat = new THREE.MeshStandardMaterial({ color: 0xe72564 });
+        var geom = new TextGeometry(name, { font: font, size: fontsize, height: fontsize / 5., });
+        var text = new THREE.Mesh(geom, mat);
         text.geometry.computeBoundingBox();
 
-        text.position.y = -text.geometry.boundingBox.max.y/2.;
-        text.position.x = -text.geometry.boundingBox.max.x/2.;
+        text.position.y = -text.geometry.boundingBox.max.y / 2.;
+        text.position.x = -text.geometry.boundingBox.max.x / 2.;
         text.position.z = 0.01;
         text.userData.url = url;
-    
-        var mat = new THREE.MeshStandardMaterial( { color: 0x333333 } );
-        var geom = new THREE.BoxGeometry( fontsize+text.geometry.boundingBox.max.x,fontsize+text.geometry.boundingBox.max.y, 0.1 );
-        background = new THREE.Mesh( geom, mat );
+
+        var mat = new THREE.MeshStandardMaterial({ color: 0x333333 });
+        var geom = new THREE.BoxGeometry(fontsize + text.geometry.boundingBox.max.x, fontsize + text.geometry.boundingBox.max.y, 0.1);
+        background = new THREE.Mesh(geom, mat);
         background.userData.url = url;
-    
+
         background.position.set(...location);
         background.add(text);
-    
+
         const type = 'button';
         background.userData.type = type; // this sets up interaction group for controllers
         text.userData.type = type; // this sets up interaction group for controllers
-    
+
         controls.interaction.selectStartHandlers[type] = CONTROLLERS.onRedirectButtonSelectStart;
         controls.interaction.selectEndHandlers[type] = CONTROLLERS.onRedirectButtonSelectEnd;
         // controls.interaction.selectableObjects.push(button);
         controls.interaction.selectableObjects.push(background);
-        
-        background.scale.set(scale,scale,scale);
-    
-        scene.add( background );
+
+        background.scale.set(scale, scale, scale);
+
+        scene.add(background);
 
         return background
     }
-    else{
+    else {
         // console.log('font not loaded, waiting...')
         setTimeout(add_url_button.bind(null, url, name, location, scale, controls, scene), 200);
     }
 }
 
 export function add_action_button(type, name, selectStartFunction, selectEndFunction, intersectionFunction, location, scale, controls, scene) {
-    if ( font !== undefined ) {
+    if (font !== undefined) {
         let background;
 
-        var mat = new THREE.MeshStandardMaterial( { color: 0xe72564 } );
-        var geom = new TextGeometry( name, { font: font, size: fontsize, height: fontsize/5., } );
-        var text = new THREE.Mesh( geom, mat );
+        var mat = new THREE.MeshStandardMaterial({ color: 0xe72564 });
+        var geom = new TextGeometry(name, { font: font, size: fontsize, height: fontsize / 5., });
+        var text = new THREE.Mesh(geom, mat);
         text.geometry.computeBoundingBox();
 
-        text.position.y = -text.geometry.boundingBox.max.y/2.;
-        text.position.x = -text.geometry.boundingBox.max.x/2.;
+        text.position.y = -text.geometry.boundingBox.max.y / 2.;
+        text.position.x = -text.geometry.boundingBox.max.x / 2.;
         text.position.z = 0.01;
 
-        var mat = new THREE.MeshStandardMaterial( { color: 0x333333 } );
-        var geom = new THREE.BoxGeometry( fontsize+text.geometry.boundingBox.max.x,fontsize+text.geometry.boundingBox.max.y, 0.1 );
-        background = new THREE.Mesh( geom, mat );
+        var mat = new THREE.MeshStandardMaterial({ color: 0x333333 });
+        var geom = new THREE.BoxGeometry(fontsize + text.geometry.boundingBox.max.x, fontsize + text.geometry.boundingBox.max.y, 0.1);
+        background = new THREE.Mesh(geom, mat);
 
         background.position.set(...location);
         background.add(text);
@@ -82,14 +83,14 @@ export function add_action_button(type, name, selectStartFunction, selectEndFunc
         controls.interaction.intersectionHandlers[type] = intersectionFunction;
         // controls.interaction.selectableObjects.push(button);
         controls.interaction.selectableObjects.push(background);
-        
-        background.scale.set(scale,scale,scale);
 
-        scene.add( background );
+        background.scale.set(scale, scale, scale);
+
+        scene.add(background);
 
         return background;
     }
-    else{
+    else {
         // console.log('font not loaded, waiting...')
         setTimeout(add_url_button.bind(null, url, name, location, scale, controls, scene), 200);
     }
--- webpack.config.js
+++ webpack.config.js
@@ -3,56 +3,61 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = [
-    {
-        mode: "development",
-        // mode: "production",
-        entry: {
-          index : './js/index.js',
-          isotropic : './js/isotropic.js',
-          triaxial : './js/triaxial.js'
-        },
-        plugins: [
-          new webpack.ProvidePlugin({
-            THREE : 'three'
-          }),
-          new HtmlWebpackPlugin({
-            title: 'NDDEM in VR',
-            // favicon: "./resources/favicon512.png",
-            template: "index.html",
-            chunks: ['index']
-          }),
-          new HtmlWebpackPlugin({
-            title: 'NDDEM in VR',
-            // favicon: "./resources/favicon512.png",
-            template: "index.html",
-            filename: "isotropic.html",
-            chunks: ['isotropic']
-          }),
-          new HtmlWebpackPlugin({
-            title: 'NDDEM in VR',
-            // favicon: "./resources/favicon512.png",
-            template: "index.html",
-            filename: "triaxial.html",
-            chunks: ['triaxial']
-          })
-        ],
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: '[name]-bundle.js',
-            clean: true,
-        },
-        devServer: {
-            static: {
-              directory: '.'
-            },
-        },
-        module: {
-            rules: [
-              {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-              },
-            ],
-          },
+  {
+    mode: "development",
+    // mode: "production",
+    entry: {
+      index: './js/index.js',
+      isotropic: './js/isotropic.js',
+      triaxial: './js/triaxial.js'
     },
+    plugins: [
+      new webpack.ProvidePlugin({
+        THREE: 'three'
+      }),
+      new HtmlWebpackPlugin({
+        title: 'NDDEM in VR',
+        // favicon: "./resources/favicon512.png",
+        template: "index.html",
+        chunks: ['index']
+      }),
+      new HtmlWebpackPlugin({
+        title: 'Isotropic compression',
+        // favicon: "./resources/favicon512.png",
+        template: "index.html",
+        filename: "isotropic.html",
+        chunks: ['isotropic']
+      }),
+      new HtmlWebpackPlugin({
+        title: 'Triaxial compression',
+        // favicon: "./resources/favicon512.png",
+        template: "index.html",
+        filename: "triaxial.html",
+        chunks: ['triaxial']
+      })
+    ],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name]-bundle.js',
+      clean: true,
+    },
+    devServer: {
+      static: {
+        directory: '.'
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(json|png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+          use: ["file-loader?name=[name].[ext]"]
+        },
+      ],
+    },
+  },
 ];
