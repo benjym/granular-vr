3b5a424a3eef8f87075a006ad09936686e25f01c
Benjy Marks <benjy.marks@sydney.edu.au>
2023-06-30 10:12:53 +1000

    Update 4d-billiards.js

--- src/4d-billiards.js
+++ src/4d-billiards.js
@@ -1,5 +1,6 @@
 import css from "../css/main.css";
-import track from "../text-to-speech/index.mp3";
+import track from "../text-to-speech/4d-billiards-hypersphere.mp3";
+import track from "../text-to-speech/4d-billiards-hypercube.mp3";
 
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
 
